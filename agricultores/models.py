a491c7459459791d49315b40acdfec57828169f0
joseedusarmento <j.eduardo.sarmento10@gmail.com>
2025-08-08 15:18:08 -0300

    criação do model Agricultor

--- agricultores/models.py
+++ agricultores/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Agricultor(models.Model):
+    nome = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    data_de_registro = models.DateTimeField(auto_now_add=True)
+    
 # Create your models here.
