81826fd85b7f95f101ea8178239be33b1e9ec156
Ernesto <149084896+ErnestoSESB@users.noreply.github.com>
2025-08-14 12:34:15 -0300

    Atualizar o views.py

--- agricultores/views.py
+++ agricultores/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Agricultor
+
+def listar_agricultores(request):
+    agricultores = Agricultor.objects.all().order_by('-data_de_registro')
+    return render(request, 'agricultores/lista.html', {'agricultores': agricultores})
